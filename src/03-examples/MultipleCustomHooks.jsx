8444455d0147931da2f8a793ea20919e784b19d0
Brayan Garcia <bgarcia.95@outlook.com>
2023-02-02 12:33:40 -0600

    add ui for MultipleCustomHooks screen

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -1,10 +1,30 @@
 import React from "react";
 
+import { LoadingQuote, Quote } from "./components";
+import { useFetch } from "../hooks";
+
 export const MultipleCustomHooks = () => {
+  const { data, isLoading, hasError, getFetch } = useFetch(
+    "https://api.breakingbadquotes.xyz/v1/quotes"
+  );
+
+  const { author, quote } = !!data && data[0];
+
   return (
     <>
       <h1>Breaking Bad Quotes</h1>
       <hr />
+
+      <LoadingQuote isLoading={isLoading} />
+      <Quote author={author} quote={quote} />
+
+      <button
+        className="btn btn-primary"
+        disabled={isLoading}
+        onClick={getFetch}
+      >
+        Next quote
+      </button>
     </>
   );
 };
