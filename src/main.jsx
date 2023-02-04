b63d7b80a2acd282ef3b066b5cfe22c5e355fc1a
Brayan Garcia <bgarcia.95@outlook.com>
2023-02-03 20:49:38 -0600

    connect app with browserrouter and provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { UserProvider } from "./10-useContext/context/UserProvider";
 
 // import { HooksApp } from "./HooksApp";
 // import { CounterApp } from "./01-useState/CounterApp";
@@ -12,15 +14,20 @@ import ReactDOM from "react-dom/client";
 // import { Memorize } from "./06-memos/Memorize";
 // import { MemoHook } from "./06-memos/MemoHook";
 // import { CallbackHook } from "./07-useCallback/CallbackHook";
-import { Padre } from "./08-tarea-memo/Padre";
-import { TodoApp } from "./09-useReducer/TodoApp";
+// import { Padre } from "./08-tarea-memo/Padre";
+// import { TodoApp } from "./09-useReducer/TodoApp";
+import { MainApp } from "./10-useContext/MainApp";
 
 // import './09-useReducer/intro-reducer';
 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // <React.StrictMode>
-  <TodoApp />
-  // </React.StrictMode>
+  <BrowserRouter>
+    {/* <React.StrictMode> */}
+    <UserProvider>
+      <MainApp />
+    </UserProvider>
+    {/* </React.StrictMode> */}
+  </BrowserRouter>
 );
